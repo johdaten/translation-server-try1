7c41f4eb4e635ee5285cddaa14e870e2eceda483
john tenorio <58564455+johdaten@users.noreply.github.com>
2022-01-04 11:31:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import 'path'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -8,7 +8,10 @@ const app = express()
 app.use(cors())
 dotenv.config()
 
-
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'))
+ })
 
 let port = process.env.PORT || 3001
 mongoose.connect(process.env.DATABASE_URL)
